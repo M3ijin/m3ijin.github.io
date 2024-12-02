2d29de7300ef180eddc1c613149662642116b9b9
M3ijin <alexolejnikcarballo@gmail.com>
2024-12-02 15:21:59 +0100

    Version Alpha (Cambios HTML/CSS/JS)

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function switchTab(tabId) {
-    // Ocultar todas las pestañas
-    const contents = document.querySelectorAll('.tab-content');
-    contents.forEach(content => content.classList.remove('active'));
+        // Cambiar entre secciones
+        function switchTab(tabId) {
+            const contents = document.querySelectorAll('.tab-content');
+            contents.forEach(content => content.classList.remove('active'));
+            const activeTab = document.getElementById(tabId);
+            if (activeTab) activeTab.classList.add('active');
+        }
 
-    // Mostrar la pestaña seleccionada
-    const activeTab = document.getElementById(tabId);
-    if (activeTab) {
-        activeTab.classList.add('active');
-    }
-}
+        // Filtrar contenido en las secciones
+        function filterContent(section) {
+            const input = document.getElementById(`search-${section}`).value.toLowerCase();
+            const grid = document.getElementById(`grid-${section}`);
+            const cards = grid.querySelectorAll('.card');
+        
+            cards.forEach(card => {
+                if (card.textContent.toLowerCase().includes(input)) {
+                    card.classList.remove('hidden'); // Muestra la carta
+                } else {
+                    card.classList.add('hidden'); // Oculta la carta
+                }
+            });
+        }
